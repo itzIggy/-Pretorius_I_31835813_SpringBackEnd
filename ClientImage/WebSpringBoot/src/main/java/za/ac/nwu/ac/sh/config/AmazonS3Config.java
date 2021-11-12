2d80c6a20728ddbe9b9e4ce9fa0ed68ac9256a7d
itzIggy <ivan7q20@gmail.com>
2021-11-12 15:10:48 +0200

    AWS S3

---
+++ ClientImage/WebSpringBoot/src/main/java/za/ac/nwu/ac/sh/config/AmazonS3Config.java
@@ -0,0 +1,25 @@
+package za.ac.nwu.ac.sh.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonS3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String accessSecret;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    /*private AmazonS3 asd(){}*/
+}
